29f5e90ac7d526bb77b047dd3380a80ede7ffede
lihungchieh <hongjie.lee888@gmail.com>
2018-09-02 23:15:47 +0800

    anther information

--- first.cpp
+++ first.cpp
@@ -6,5 +6,7 @@ int main(int argc, char* argv[]){
 	cv::Mat img;
 	img = cv::imread("/home/lihongjie/Pictures/first.png", 1);
 	std::cout << img.rows << " " << img.cols << "\n";
+	std::cout << "Little Tiger is the best tiger\n";
+	std::cout << "Big Cat Cat is the best cat\n";
 	return 0;
 }
